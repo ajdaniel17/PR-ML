17c7fee7873aa4aa5695326b0fe5bbf1e91cbebe
ajdaniel17 <ajdaniel17@yahoo.com>
2022-01-28 15:15:18 -0600

    Did a lot

--- Machine Learning/Project 1/Test.py
+++ Machine Learning/Project 1/Test.py
@@ -24,49 +24,38 @@ class car():
     def setHorsePower(self,H):
         self.HorsePower = H
 
-#Open the Excel sheet
-ED = pd.read_excel("proj1Dataset.xlsx")
-index = ED.index
-rows = len(index)
+car1 = car(3,6)
+car2 = car(5,4)
+car3 = car(4,2)
 
-#Initialize Empty List
-Data = []
-
-# Fill the array with the cars class, skip points of data with missing parts
-for i in range(rows):
-    if(pd.isna(ED.iloc[i,0]) or pd.isna(ED.iloc[i,1])):
-        print("Data Missing! Skipping row " , i + 2)
-    else:
-        TempCar = car(ED.iat[i,0], ED.iat[i,1])
-        Data.append(TempCar)
-        #print(Data[i].getHorsePower())
-        #print(Data[i].getWeight())
-
+Data = [car1,car2,car3]
 
 #Code to do ML HERE
+x = np.empty((0,1),int)
+for i in range(len(Data)):
+    x = np.append(x,np.array([[Data[i].getWeight()]]))
 
-np.set_printoptions(threshold=np.inf)
+
 bigX = np.empty((0,2),int)
 for i in range(len(Data)):
     bigX = np.append(bigX,np.array([[Data[i].getWeight(),1]]),0)
 #print(bigX)
-#w = np.matmul(np.linalg.pinv(np.matmul(bigX.transpose(),bigX)),bigX.transpose())
-#print(w.shape)
-w = np.linalg.pinv(bigX)
+
 t = np.empty((0,1),int)
 for i in range(len(Data)):
     t = np.append(t,np.array([[Data[i].getHorsePower()]]))
-#print(t.shape)
+#print(t)
 
+w = np.linalg.pinv(bigX)
 W = np.matmul(w,t)
 #print(W)
 
 Y = np.matmul(bigX,W)
-print(Y.shape)
+print(Y)
 #Plot First Figure : Closed Form
 plt.figure(1)
+plt.plot(x,Y)
 
-plt.draw()
 for i in range(len(Data)):
     plt.plot(Data[i].getWeight(),Data[i].getHorsePower(),'rx')
 
@@ -75,14 +64,5 @@ plt.xlabel('Weight')
 plt.ylabel('Horsepower')
 plt.title("""Matlab's "carbig" dataset""")
 
-#Plot Second Figure : Gradient Descent
-# plt.figure(2)
-# for i in range(len(Data)):
-#     plt.plot(Data[i].getWeight(),Data[i].getHorsePower(),'rx')
-
-
-# plt.xlabel('Weight')
-# plt.ylabel('Horsepower')
-# plt.title("""Matlab's "carbig" dataset""")
-
+plt.draw()
 plt.show()
