799413240636c5d36d92cbdf13afd86d18487112
ajdaniel17 <ajdaniel17@yahoo.com>
2022-02-04 15:39:28 -0600

    Thing

--- Pattern Recognition/Project 1 PR/Daniel_Aidan_Project1.py
+++ Pattern Recognition/Project 1 PR/Daniel_Aidan_Project1.py
@@ -23,6 +23,38 @@ class plant():
     def getM4(self):
         return self.M4
 
+    def getName(self):
+        return self.name
+
+def findInfo(data,M,name,ty):
+    temp = np.empty(0,float)
+    for i in range(len(data)):
+       
+        if (data[i].getName() == name or name == "all"):
+            if M == 1:
+                temp = np.append(temp,(data[i].getM1()))
+            elif M == 2:
+                temp = np.append(temp,(data[i].getM2()))
+            elif M == 3:
+                temp = np.append(temp,(data[i].getM3()))
+            elif M == 4:
+                temp = np.append(temp,(data[i].getM4()))
+            else:
+                print("ERROR WRONG M")
+                return
+    if ty == 1:
+        return np.amin(temp)
+    elif ty == 2:
+        return np.amax(temp)
+    elif ty == 3:
+        return np.average(temp)
+    elif ty == 4:
+        return np.var(temp)
+    else:
+        print("ERROR WRONG TY")
+        return
+    return
+
 #Open the Excel sheet
 ED = pd.read_excel("Proj1DataSet.xlsx")
 index = ED.index
@@ -37,5 +69,12 @@ for i in range(rows):
     else:
         TempPlant = plant(ED.iat[i,0], ED.iat[i,1], ED.iat[i,2], ED.iat[i,3], ED.iat[i,4])
         Data.append(TempPlant)
-        print(Data[i].getM1())
-        #print(Data[i].getWeight())
+        #print(Data[i].getName())
+
+try:
+    print("Min is ",findInfo(Data, 1, "setosa", 1))
+    print("Max is ",findInfo(Data, 1, "setosa", 2))
+    print("Average is ",findInfo(Data, 1, "setosa", 3))
+    print("Variance is ",findInfo(Data, 1, "setosa", 4))
+except ValueError:
+    pass
