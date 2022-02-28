8cdc283f4637bacc3f14b1ae688e42b2ac8ab051
ajdaniel17 <ajdaniel17@yahoo.com>
2022-02-28 02:40:57 -0600

    huh

--- Machine Learning/Project 3 ML/Daniel_Aidan_Project3.py
+++ Machine Learning/Project 3 ML/Daniel_Aidan_Project3.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 np.random.seed(69)
 Pi = np.pi
@@ -20,10 +22,10 @@ for i in range(L):
     EX1 = np.random.normal(SD,np.mean(X[i][:]),N)
     temp = np.sin(2*Pi*X[i][:])  + EX1
     T = np.append(T,np.array([temp]),0)
-
+#print(T.shape)
 M = 20
 
-Phi = np.empty((N,M+1,0),float)
+Phi = np.empty((0,N,M+1),float)
 
 Mu = np.random.uniform(0,1,M)
 Mu = np.sort(Mu)
@@ -42,9 +44,48 @@ for i in range(L):
                 temp = np.append(temp,np.array([temp3]))
         temp2 = np.append(temp2,np.array([temp]),0)
     #print(temp2.shape)
-    Phi = np.dstack((Phi,temp2))
-print(Phi.shape)
+    Phi = np.append(Phi,np.array([temp2]),0)
+#print(Phi.shape)
+#print(Phi[0])
 
-l = np.random.uniform(.01,5,1000)
-l = np.sort(l)
+lam = np.random.uniform(.01,5,10)
+lam = np.sort(lam)
 
+I = np.identity(M+1)
+W = np.empty((0,L,(M+1)),float)
+# Setup all Weight Vectors
+
+for i in range(len(lam)):
+    temp1 = np.empty((0,(M+1)),float)
+    for j in range(L):
+        temp =  np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(Phi[L-1][:][:]),Phi[L-1][:][:])+lam[i]*I),np.transpose(Phi[L-1][:][:])),T[j][:])
+        #print(temp.shape)
+        temp1 = np.append(temp1,np.array([temp]),0)
+    W = np.append(W,np.array([temp1]),0)
+#print(W.shape)
+
+Xrange = np.linspace(0, 1,500)
+
+
+temp2 = np.empty((0,(M+1)),float)
+for j in range(len(Xrange)):
+        temp = np.empty((0),float)
+        for k in range(M+1):
+            if k == 0:
+                temp = np.append(temp,np.array([1]))
+            else:
+                temp3 = np.exp(-1*((Xrange[j]-Mu[k-1])**2)/(2*(s**2)))
+                temp = np.append(temp,np.array([temp3]))
+        temp2 = np.append(temp2,np.array([temp]),0)
+
+
+plt.figure(1)
+
+for i in range(N):
+    for j in range(L):
+        plt.plot(X[j][i],T[j][i],'bo')
+# for i in range(1):
+#     plt.plot(X[i][:],np.matmul(Phi[i][:][:],W[0][i][:]))
+for i in range(20):
+    plt.plot(Xrange,np.matmul(temp2,W[9][i][:]),'r')
+plt.show()
