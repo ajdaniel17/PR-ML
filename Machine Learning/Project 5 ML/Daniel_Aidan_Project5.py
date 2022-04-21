bee17eb3e851f1c653aa7a714f5652b24194bdf2
ajdaniel17 <ajdaniel17@yahoo.com>
2022-04-20 21:44:02 -0500

    Finished Project 5

--- Machine Learning/Project 5 ML/Daniel_Aidan_Project5.py
+++ Machine Learning/Project 5 ML/Daniel_Aidan_Project5.py
@@ -32,6 +32,7 @@ W1 = np.random.rand(2,2)
 b1 = np.random.rand(2)
 
 plt.figure(1)
+plt.title("Epochs vs Error: XOR")
 graphX = []
 graphY = []
 for m in range(3000):
@@ -67,7 +68,7 @@ for m in range(3000):
     #Graph Error
     graphX.append(m)
     graphY.append(E)
-    if m % 100 == 0:
+    if m % 500 == 0:
         plt.ion()
         plt.xlabel("Epochs")
         plt.ylabel("Error")
@@ -79,13 +80,14 @@ for m in range(3000):
 plt.ioff()
 plt.figure(1)
 plt.xlabel("Epochs")
+plt.title("Epochs vs Error: XOR")
 plt.ylabel("Error")
 plt.xlim(0,3001)
 plt.semilogy(graphX,graphY)
 
 plt.figure(2)
 ax = plt.axes(projection='3d')
-
+plt.title("XOR Decision Surface")
 X1 = np.linspace(-2,2,50)
 X2 = np.linspace(-2,2,50)
 Xgraph, Ygraph = np.meshgrid(X1, X2)
@@ -105,4 +107,196 @@ ax.scatter(-1,-1,-1,color = 'r',s=200,alpha=1)
 ax.scatter(1,-1,1,color = 'b',s=200,alpha=1)
 ax.scatter(-1,1,1,color = 'b',s=200,alpha=1)
 
+#Load Data From Excel
+ED = pd.read_excel("Proj5DataSet.xlsx")
+index = ED.index
+rows = len(index)
+DataX = np.empty((0,1),float)
+DataC = np.empty((0),float)
+for i in range(rows):
+    if(pd.isna(ED.iloc[i,0]) or pd.isna(ED.iloc[i,1])):
+        print("Data Missing! Skipping row " , i + 2)
+    else:
+        DataX = np.append(DataX,np.array([[ED.iat[i,0]]]),0)
+        DataC = np.append(DataC, ED.iat[i,1])
+
+N,D = DataX.shape
+L = 1
+U = 3
+W0 = np.random.rand(U,1)
+b0 = np.random.rand(U)
+W1 = np.random.rand(1,U)
+b1 = np.random.rand(1)
+LR = .2
+B = .9
+prevV1 = 0
+prevVb1 = 0
+prevV0 = 0
+prevVb0 = 0
+plt.figure(3)
+plt.title("Epochs vs Error: 3 Units")
+graphX = []
+graphY = []
+for m in range(3000):
+    #Forward Pass Setup
+    
+    B0 = np.empty((0,U),float)
+    for i in range(N):
+        B0 = np.vstack((B0,b0))
+    B0 = B0.T
+    
+    B1 = np.empty((0,1),float)
+    for i in range(N):
+        B1 = np.vstack([B1,b1])
+    B1 = B1.T
+
+    #Forward Pass
+    A0 = np.dot(W0,DataX.T) + B0
+    Z0 = np.tanh(A0)
+
+    A1 = np.dot(W1,Z0) + B1
+    Y = np.tanh(A1)
+
+    #Back Pass
+    L = Y-DataC.T
+
+    E = .5 * np.sum(np.linalg.norm(L,axis=0)**2)
+    V1 = (prevV1 * B) + ((1-B)* np.dot(L,Z0.T))
+    Vb1 = (prevVb1 * B) + ((1-B)* np.mean(L,axis=1))
+    W1 = W1 - LR*V1
+    b1 = b1 - LR*Vb1
+  
+    delta1 = np.multiply(np.dot(W1.T,L),(1-(Z0**2)))
+    V0 = (prevV0 * B) + ((1-B)* np.dot(delta1,DataX))
+    Vb0 = (prevVb0 * B) + ((1-B)* np.mean(L,axis=1))
+    W0 = W0 - LR*V0
+    b0 = b0 - LR*Vb0
+    
+    prevV1 = V1
+    prevVb1 = Vb1
+    prevV0 = V0
+    prevVb0 = Vb0
+    #Graph Error
+    graphX.append(m)
+    graphY.append(E)
+    if m % 500 == 0:
+        plt.ion()
+        plt.xlabel("Epochs")
+        plt.ylabel("Error")
+        plt.xlim(0,3001)
+        plt.xticks(np.arange(0,3001,500))
+        plt.semilogy(graphX,graphY)
+        plt.pause(.001)
+        plt.clf()
+plt.ioff()
+plt.figure(3)
+plt.title("Epochs vs Error: 3 Units")
+plt.xlabel("Epochs")
+plt.ylabel("Error")
+plt.xlim(0,3001)
+plt.semilogy(graphX,graphY)
+
+plt.figure(4)
+plt.xlabel("X")
+plt.ylabel("T")
+title = '3 Units, Loss: ' + str(E)
+plt.title(title)
+X1 = np.linspace(-1,1,100)
+Y = np.empty(100)
+for i in range(100):
+    A0 = np.dot((W0),X1[i]) + b0.reshape((U,1))
+    Z0 = np.tanh(A0)
+    A1 = np.dot(W1,Z0) + b1
+    Y[i] = np.tanh(A1)
+plt.plot(X1,Y)
+plt.scatter(DataX,DataC)
+
+L = 1
+U = 20
+W0 = np.random.rand(U,1)
+b0 = np.random.rand(U)
+W1 = np.random.rand(1,U)
+b1 = np.random.rand(1)
+LR = .05
+prevV1 = 0
+prevVb1 = 0
+prevV0 = 0
+prevVb0 = 0
+B = .9
+plt.figure(5)
+plt.title("Epochs vs Error: 20 Units")
+graphX = []
+graphY = []
+for m in range(3000):
+    #Forward Pass Setup
+    
+    B0 = np.empty((0,U),float)
+    for i in range(N):
+        B0 = np.vstack((B0,b0))
+    B0 = B0.T
+    
+    B1 = np.empty((0,1),float)
+    for i in range(N):
+        B1 = np.vstack([B1,b1])
+    B1 = B1.T
+
+    #Forward Pass
+    A0 = np.dot(W0,DataX.T) + B0
+    Z0 = np.tanh(A0)
+
+    A1 = np.dot(W1,Z0) + B1
+    Y = np.tanh(A1)
+
+    #Back Pass
+    L = Y-DataC.T
+    E = .5 * np.sum(np.linalg.norm(L,axis=0)**2)
+    V1 = (prevV1 * B) + ((1-B)* np.dot(L,Z0.T))
+    Vb1 = (prevVb1 * B) + ((1-B)* np.mean(L,axis=1))
+    W1 = W1 - LR*V1
+    b1 = b1 - LR*Vb1
+  
+    delta1 = np.multiply(np.dot(W1.T,L),(1-(Z0**2)))
+    V0 = (prevV0 * B) + ((1-B)* np.dot(delta1,DataX))
+    Vb0 = (prevVb0 * B) + ((1-B)* np.mean(L,axis=1))
+    W0 = W0 - LR*V0
+    b0 = b0 - LR*Vb0
+    
+    prevV1 = V1
+    prevVb1 = Vb1
+    prevV0 = V0
+    prevVb0 = Vb0
+    #Graph Error
+    graphX.append(m)
+    graphY.append(E)
+    if m % 500 == 0:
+        plt.ion()
+        plt.xlabel("Epochs")
+        plt.ylabel("Error")
+        plt.xlim(0,3001)
+        plt.xticks(np.arange(0,3001,500))
+        plt.semilogy(graphX,graphY)
+        plt.pause(.001)
+        plt.clf()
+plt.ioff()
+plt.figure(5)
+plt.xlabel("Epochs")
+plt.title("Epochs vs Error: 20 Units")
+plt.ylabel("Error")
+plt.xlim(0,3001)
+plt.semilogy(graphX,graphY)
+
+plt.figure(6)
+plt.xlabel("X")
+plt.ylabel("T")
+title = '20 Units, Loss: ' + str(E)
+plt.title(title)
+X1 = np.linspace(-1,1,100)
+Y = np.empty(100)
+for i in range(100):
+    A0 = np.dot((W0),X1[i]) + b0.reshape((U,1))
+    Z0 = np.tanh(A0)
+    A1 = np.dot(W1,Z0) + b1
+    Y[i] = np.tanh(A1)
+plt.plot(X1,Y)
+plt.scatter(DataX,DataC)
 plt.show()
